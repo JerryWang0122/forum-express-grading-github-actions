70ebd707b0d1d8eb4412443252da495d296af691
JerryWang0122 <school832@gmail.com>
2023-12-28 11:11:38 +0800

    feat: R03 pass, User profile
    1. 瀏覽profile(R03-1 pass)
    2. add image to user model
    3. 瀏覽編輯 Profile 頁面(R03-2 pass)
    4. 編輯 Profile (R03-3 pass)

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ const userController = require('../controllers/user-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const commentController = require('../controllers/comment-controller')
+const upload = require('../middleware/multer')
 
 router.use('/admin', authenticatedAdmin, admin)
 
@@ -19,6 +20,10 @@ router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
+
+router.get('/users/:id/edit', authenticated, userController.editUser)
+router.get('/users/:id', authenticated, userController.getUser)
+router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
 
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
